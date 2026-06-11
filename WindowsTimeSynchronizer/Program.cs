70471f76dad6106b4c1f264984ae7cbf06ddd2e6
0cin05 <widawski.nico@gmail.com>
2026-06-11 20:41:04 +0200

    Request administrator privileges on startup

--- WindowsTimeSynchronizer/Program.cs
+++ WindowsTimeSynchronizer/Program.cs
@@ -1,4 +1,20 @@
 ﻿using System.Diagnostics;
+using System.Security.Principal;
+using static System.Security.Principal.WindowsBuiltInRole;
+
+var identity = WindowsIdentity.GetCurrent();
+var principal = new WindowsPrincipal(identity);
+
+if (!principal.IsInRole(Administrator))
+{
+    var exePath = Environment.ProcessPath!;
+    Process.Start(new ProcessStartInfo(exePath)
+    {
+        UseShellExecute = true,
+        Verb = "runas"
+    });
+    return;
+}
 
 Process.Start("net", "start w32time").WaitForExit();
 Process.Start("w32tm", "/resync /force").WaitForExit();
